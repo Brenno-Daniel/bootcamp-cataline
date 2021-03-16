fd4cb129bbd437f93cf5f2c856ab4afe5fd45b51
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-03-16 20:25:07 -0300

    add cors on backend

--- intro-fullstack/backend/src/server.ts
+++ intro-fullstack/backend/src/server.ts
@@ -1,9 +1,11 @@
 import express, { request, response } from 'express'
+import cors from 'cors'
 import { v4 as uuid } from 'uuid'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 interface User {
     id: string
