270070fb37d1322dacd51fb5a1465e98f0e05b86
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-03-17 20:32:18 -0300

    inital config

--- vue-key-features/frontend/src/App.vue
+++ vue-key-features/frontend/src/App.vue
@@ -1,27 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>Helloooo {{ person.name }}</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+
+interface Person {
+  name: string
+  age: number
+}
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
+  data() {
+    return {
+      person: {} as Person
+    }
+  }
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
