b5b4636f366032e7bc6503a98898d62d31a16f54
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-04-13 15:04:49 -0300

    create personalizatedDirective vue component

---
+++ vue-key-features/frontend/src/components/03-directives/personalizatedDirective.vue
@@ -0,0 +1,16 @@
+<template>
+    <!-- Construindo diretiva personalizada -->
+    <h1 v-highlight:background.shadow="'blue'">Diretiva Personalizada</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style scoped>
+h1 {
+    color: white;
+}
+</style>
