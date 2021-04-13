491ad27b2e35a7c6916d160846cab386c87b7ae1
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-04-13 18:28:09 -0300

    first component ButtonStyled.vue

--- vue-key-features/frontend/src/App.vue
+++ vue-key-features/frontend/src/App.vue
@@ -1,11 +1,14 @@
 <template>
-    <div></div>
+    <ButtonStyled />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import ButtonStyled from '@/components/05-componentization/ButtonStyled.vue';
 
-export default defineComponent({});
+export default defineComponent({
+    components: { ButtonStyled },
+});
 </script>
 
 <style scoped></style>
