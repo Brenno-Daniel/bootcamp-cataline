5897922533f6c67ee35ae007748aa25a0eaff91f
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-05-04 20:38:35 -0300

    working with Dynamic Components

--- vue-key-features/frontend/src/App.vue
+++ vue-key-features/frontend/src/App.vue
@@ -1,8 +1,12 @@
 <template>
     <!-- Trabalhando com Props e estilização de componentes filhos -->
+    <h3>Trabalhando com Props e estilização de componentes filhos:</h3>
     <ButtonStyled text="Cadastro" color="primary" :user="user" />
     <ButtonStyled text="Login" color="danger" :user="user" />
+    <!-- Trabalhando com Props e estilização de componentes filhos -->
+    <hr />
     <!-- Trabalhando com Slots -->
+    <h3>Trabalhando com Slots:</h3>
     <ButtonStyled text="Facebook" :user="user">
         <template v-slot:before>
             <i class="fab fa-facebook-square"></i>
@@ -11,21 +15,41 @@
             <i class="fab fa-facebook"></i>
         </template>
     </ButtonStyled>
+    <!-- Trabalhando com Slots -->
+    <hr />
+    <!-- Trabalhando com Events -->
+    <h3>Trabalhando com Events:</h3>
     <ButtonStyled text="Botão" @evento="action($event)" :user="user" />
+    <!-- Trabalhando com Events -->
+    <hr />
+    <!-- Trabalhando com Dynamic Componentes [Componentes Dinâmicos] -->
+    <h3>Componentes dinamicos:</h3>
+    <ul>
+        <li @click="currentView = 'Home'">Home</li>
+        <li @click="currentView = 'About'">Sobre</li>
+        <li @click="currentView = 'Contact'">Contato</li>
+    </ul>
+    <component :is="currentView" />
+    <!-- Trabalhando com Dynamic Componentes [Componentes Dinâmicos] -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 import ButtonStyled from '@/components/05-componentization/ButtonStyled.vue';
+import Home from '@/pages/Home.vue';
+import About from '@/pages/About.vue';
+import Contact from '@/pages/Contact.vue';
 
 export default defineComponent({
-    components: { ButtonStyled },
+    components: { ButtonStyled, Home, About, Contact },
     data() {
         return {
             user: {
                 id: 1,
                 name: 'Brenno',
             },
+            currentView: 'Home',
         };
     },
     methods: {
@@ -40,10 +64,6 @@ export default defineComponent({
 .button-styled {
     position: relative;
     margin-top: 5%;
-}
-
-.fb-button-margin {
-    margin-top: 10%;
 }
 
 /* v-deep adiciona uma estilização profunda para um componente */
