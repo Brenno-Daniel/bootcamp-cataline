86d84be51673352f9df78c853aba89b56230e87d
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-05-05 23:42:18 -0300

    working with transition, custom, hooks and appear

--- vue-key-features/frontend/src/App.vue
+++ vue-key-features/frontend/src/App.vue
@@ -1,125 +1,32 @@
 <template>
-    <!-- Aula 5 - Componentização -->
-    <!-- Trabalhando com Props e estilização de componentes filhos -->
-    <h3>Trabalhando com Props e estilização de componentes filhos:</h3>
-    <ButtonStyled text="Cadastro" color="primary" :user="user" />
-    <ButtonStyled text="Login" color="danger" :user="user" />
-    <!-- Trabalhando com Props e estilização de componentes filhos -->
-    <hr />
-    <!-- Trabalhando com Slots -->
-    <h3>Trabalhando com Slots:</h3>
-    <ButtonStyled text="Facebook" :user="user">
-        <template v-slot:before>
-            <i class="fab fa-facebook-square"></i>
-        </template>
-        <template v-slot:after>
-            <i class="fab fa-facebook"></i>
-        </template>
-    </ButtonStyled>
-    <!-- Trabalhando com Slots -->
-    <hr />
-    <!-- Trabalhando com Events -->
-    <h3>Trabalhando com Events:</h3>
-    <ButtonStyled text="Botão" @evento="action($event)" :user="user" />
-    <!-- Trabalhando com Events -->
-    <hr />
-    <!-- Trabalhando com Dynamic Componentes [Componentes Dinâmicos] -->
-    <h3>Componentes dinamicos:</h3>
+    <h3>Selecione a aula:</h3>
     <ul>
         <li @click="currentView = 'Home'">Home</li>
-        <li @click="currentView = 'About'">Sobre</li>
-        <li @click="currentView = 'Contact'">Contato</li>
+        <li @click="currentView = 'Componentization'">Aula 5</li>
+        <li @click="currentView = 'Transition'">Aula 6</li>
     </ul>
-    <component :is="currentView" />
-    <!-- Trabalhando com Dynamic Componentes [Componentes Dinâmicos] -->
-    <hr />
-    <!-- Aula 5 - Componentização -->
-    <!-- ------//------------//------------//------------//------ -->
-    <!-- Aula 6 - Transições -->
-    <!-- Trabalhando com Transition -->
-    <h3>Trabalhando com Transition:</h3>
-    <button @click="show = !show">Toggle</button>
 
-    <transition name="fade-fast">
-        <h1 v-if="show">Cataline</h1>
+    <transition enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+        <component :is="currentView" />
     </transition>
-
-    <transition name="fade-slow">
-        <h1 v-if="show">Cataline</h1>
-    </transition>
-    <!-- Trabalhando com Transition -->
-    <!-- Aula 6 - Transições -->
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import 'animate.css';
 
-import ButtonStyled from '@/components/05-componentization/ButtonStyled.vue';
 import Home from '@/pages/Home.vue';
-import About from '@/pages/About.vue';
-import Contact from '@/pages/Contact.vue';
+import Componentization from '@/components/05-componentization/Componentization.vue';
+import Transition from '@/components/06-Transitions/Transition.vue';
 
 export default defineComponent({
-    components: { ButtonStyled, Home, About, Contact },
+    components: { Home, Componentization, Transition },
     data() {
         return {
-            user: {
-                id: 1,
-                name: 'Brenno',
-            },
             currentView: 'Home',
-            show: false,
         };
-    },
-    methods: {
-        action(event: string) {
-            console.log(event);
-        },
     },
 });
 </script>
 
-<style scoped>
-.button-styled {
-    position: relative;
-    margin-top: 5%;
-}
-
-/* v-deep adiciona uma estilização profunda para um componente */
-::v-deep.button-styled span {
-    color: #000;
-}
-
-/* 
-Aula 6 - Transições - Trabalhando com Transition:
-
-v-enter-from (de onde estar vindo)
-v-enter-active (como vai acontecer)
-v-enter-to (para onde está indo)
-
-v-leave-from (de onde estar vindo)
-v-leave-active (como vai acontecer)
-v-leave-to (para onde está indo)
-
-O nome inicial `v` depende do atributo `name`
-*/
-.fade-fast-enter-from,
-.fade-fast-leave-to {
-    opacity: 0;
-}
-
-.fade-fast-enter-active,
-.fade-fast-leave-active {
-    transition: opacity 0.5s;
-}
-
-.fade-slow-enter-from,
-.fade-slow-leave-to {
-    opacity: 0;
-}
-
-.fade-slow-enter-active,
-.fade-slow-leave-active {
-    transition: opacity 2s;
-}
-</style>
+<style scoped></style>
---
+++ vue-key-features/frontend/src/components/06-Transitions/Transition.vue
@@ -0,0 +1,115 @@
+<template>
+    <!-- Aula 6 - Transições -->
+    <!-- Trabalhando com Transition -->
+    <h3>Trabalhando com Transition:</h3>
+    <button @click="show = !show">Toggle</button>
+
+    <transition name="fade-slow">
+        <h1 v-if="show">Cataline</h1>
+    </transition>
+
+    <transition name="fade-fast">
+        <h1 v-show="show">Cataline</h1>
+    </transition>
+    <!-- Trabalhando com Transition -->
+    <!-- Trabalhando com Custom -->
+    <!-- Como adicionar nome de classes personalizadas -->
+    <transition enter-active-class="entrada" leave-active-class="saida">
+        <h1 v-show="show">Cataline</h1>
+    </transition>
+    <!-- Utilizando biblioteca css Animate.css -->
+    <transition enter-active-class="animate__animated animate__fadeIn" leave-active-class="animate__animated animate__fadeOut">
+        <h1 v-show="show">Cataline</h1>
+    </transition>
+    <!-- Trabalhando com Custom -->
+    <!-- Trabalhando com Hooks -->
+    <!-- <transition ...
+        @before-enter="beforeEnter"
+        @enter="enter"
+        @after-enter="afterEnter"
+        @enter-cancelled="enterCancelled"
+        @before-leave="beforeLeave"
+        @leave="leave"
+        @after-leave="afterLeave"
+        @leave-cancelled="leaveCancelled"
+        :css="false"
+    > -->
+    <transition enter-active-class="animate__animated animate__slideInLeft" leave-active-class="animate__animated animate__slideOutLeft" @after-enter="afterEnter">
+        <h1 v-show="show">Cataline</h1>
+    </transition>
+    <!-- Trabalhando com Hooks -->
+    <!-- Trabalhando com Appear -->
+    <!-- Ao adicionar o appear ao carregar a página o conteúdo é renderizado com a animação -->
+    <transition enter-active-class="animate__animated animate__slideInLeft" leave-active-class="animate__animated animate__slideOutLeft" appear>
+        <h1 v-if="!show">Cataline</h1>
+    </transition>
+    <!-- Trabalhando com Appear -->
+    <!-- Aula 6 - Transições -->
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import 'animate.css';
+
+export default defineComponent({
+    data() {
+        return {
+            show: false,
+        };
+    },
+    methods: {
+        afterEnter(element: HTMLElement) {
+            element.classList.add('test');
+        },
+    },
+});
+</script>
+
+<style scoped>
+/* 
+Aula 6 - Transições - Trabalhando com Transition:
+
+v-enter-from (de onde estar vindo)
+v-enter-active (como vai acontecer)
+v-enter-to (para onde está indo)
+
+v-leave-from (de onde estar vindo)
+v-leave-active (como vai acontecer)
+v-leave-to (para onde está indo)
+
+O nome inicial `v` depende do atributo `name`
+*/
+.fade-fast-enter-from,
+.fade-fast-leave-to {
+    opacity: 0;
+}
+
+.fade-fast-enter-active,
+.fade-fast-leave-active {
+    transition: opacity 0.5s;
+}
+
+.fade-slow-enter-from,
+.fade-slow-leave-to {
+    opacity: 0;
+}
+
+.fade-slow-enter-active,
+.fade-slow-leave-active {
+    transition: opacity 1s;
+}
+
+.entrada { 
+    background-color: green;
+    transition: opacity 0.4s;
+}
+
+.saida {
+    background-color: red;
+    transition: opacity 0.4s;
+}
+
+.test {
+    background: green;
+}
+</style>
