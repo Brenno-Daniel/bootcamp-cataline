72534fb25005e787b0af065e88bab0fc71d35cac
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-08-01 18:24:48 -0300

    init project restructuration

--- intro-vuex/faq-vuex/src/App.vue
+++ intro-vuex/faq-vuex/src/App.vue
@@ -1,14 +1,48 @@
 <template>
-    <Home />
+    <div class="app">
+        <div class="container">
+            <transition class="slider" :name="transitionType" mode="out-in">
+                <component :is="$currentComponent" />
+            </transition>
+        </div>
+    </div>
 </template>
 
 <script>
-import Home from '@/components/Home.vue';
+import FaqCategories from '@/components/FaqCategories';
+import Questions from '@/components/Questions';
+import Answer from '@/components/Answer';
 
 export default {
     name: 'App',
     components: {
-        Home,
+        FaqCategories,
+        Questions,
+        Answer,
+    },
+    data() {
+        return {
+            transitionType: 'slide-left',
+        };
+    },
+    computed: {
+        $currentComponent() {
+            return this.$store.getters.$currentComponent;
+        },
+        $transitionDepth() {
+            return this.$store.getters.$transitionDepth;
+        },
+    },
+    watch: {
+        $$transitionDepth(newValue, oldValue) {
+            if (newValue > oldValue) {
+                this.transitionType = 'slide-left';
+            } else {
+                this.transitionType = 'slide-right';
+            }
+        },
     },
 };
 </script>
+
+<style></style>
