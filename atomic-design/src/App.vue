4799c4f9fb307a4faabd39e6a1076a5f46c321ef
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-09-07 20:47:07 -0300

    molecule: Service.vue

--- atomic-design/src/App.vue
+++ atomic-design/src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="component">
-    <SocialNetworks />
+    <Service
+      :title="'Front-end'"
+      :description="'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque obcaecati laboriosam ex nihil tenetur maxime! Veniam, voluptatum quos. Eius cum molestias, id tenetur hic mollitia dicta enim dignissimos tempore laudantium!'"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { SocialNetworks } from '@/components/molecules';
+import { Service } from '@/components/molecules';
 
 export default defineComponent({
-  components: { SocialNetworks },
+  components: { Service },
 });
 </script>
 
