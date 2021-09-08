a2ea372747597f6b7b8fa26901503757772380cd
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-09-07 21:30:26 -0300

    bosons: Container.vue

--- atomic-design/src/App.vue
+++ atomic-design/src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="component">
-    <Services />
+    <Container>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, ipsam
+        quaerat illum aspernatur sit quas distinctio iste enim, et vel amet modi
+        delectus in error, tenetur cum ut veritatis deserunt.
+      </p>
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Services } from '@/components/organisms';
+import { Container } from '@/components/bosons';
 
 export default defineComponent({
-  components: { Services },
+  components: { Container },
 });
 </script>
 
