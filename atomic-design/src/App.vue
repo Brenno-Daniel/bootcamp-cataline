63aa68aa267a0c6d9192df59775426da4ad9e073
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-09-07 21:03:53 -0300

    organisms: Header.vue

--- atomic-design/src/App.vue
+++ atomic-design/src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div class="component">
-    <Service
-      :title="'Front-end'"
-      :description="'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque obcaecati laboriosam ex nihil tenetur maxime! Veniam, voluptatum quos. Eius cum molestias, id tenetur hic mollitia dicta enim dignissimos tempore laudantium!'"
-    />
+    <Header />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Service } from '@/components/molecules';
+import { Header } from '@/components/organisms';
 
 export default defineComponent({
-  components: { Service },
+  components: { Header },
 });
 </script>
 
