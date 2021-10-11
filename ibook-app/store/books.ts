d57e2ccbaf685975aafc277bc37a49e57071641f
Brenno Daniel <brenno.daniel.98@gmail.com>
2021-10-11 18:04:53 -0300

    list of books

--- ibook-app/store/books.ts
+++ ibook-app/store/books.ts
@@ -31,7 +31,6 @@ export default class Books extends VuexModule {
   @Action
   public async index() {
     const books = await $axios.$get('/books')
-    console.log(books)
     this.context.commit('SET_ALL', books)
   }
 
